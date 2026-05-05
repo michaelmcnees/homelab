5d5616e2e34ee2073d8fa3a7abaaeed83355ece4
Michael McNees <mmcnees@Michaels-MacBook-Pro.local>
2026-05-04 20:17:53 -0400

    fix: adopt existing Traefik port forwards

--- terraform/unifi/port_forwards.tf
+++ terraform/unifi/port_forwards.tf
@@ -1,12 +1,12 @@
 locals {
   traefik_external_port_forwards = {
     http = {
-      name         = "Traefik HTTP"
+      name         = "HTTP"
       wan_port     = "80"
       forward_port = "81"
     }
     https = {
-      name         = "Traefik HTTPS"
+      name         = "HTTPS"
       wan_port     = "443"
       forward_port = "444"
     }
@@ -16,9 +16,11 @@ locals {
 resource "unifi_port_forward" "traefik_external" {
   for_each = local.traefik_external_port_forwards
 
-  name     = each.value.name
-  protocol = "tcp"
-  dst_port = each.value.wan_port
-  fwd_ip   = "10.0.10.200"
-  fwd_port = each.value.forward_port
+  name                   = each.value.name
+  protocol               = "tcp"
+  port_forward_interface = "wan"
+  dst_port               = each.value.wan_port
+  fwd_ip                 = "10.0.10.200"
+  fwd_port               = each.value.forward_port
+  log                    = true
 }
